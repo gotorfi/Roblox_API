7fd8bcbed2f074c186d04f79135bb9eab81cfd71
gotorfi <gotorfi@gmail.com>
2026-05-30 12:17:51 +0300

    testing

--- app.py
+++ app.py
@@ -1,19 +1,38 @@
 from flask import Flask, request, jsonify
 import json
 import os
+import requests
+from datetime import datetime
 
 app = Flask(__name__)
 
 DATA_FILE = "analytics.json"
+DISCORD_WEBHOOK_URL = "https://discord.com/api/webhooks/1510203543783473214/VeZPvBnybCrTk4IYmOW78hSfQ5IF9jOXWvY_W23hJXKnlqfTbU-OH4xi7WURayszkmhw"
 
 if not os.path.exists(DATA_FILE):
     with open(DATA_FILE, "w") as f:
         json.dump([], f)
+def send_discord_message(message):
+
+    try:
+        requests.post(
+            DISCORD_WEBHOOK_URL,
+            json={
+                "content": message
+            },
+            timeout=10
+        )
+
+    except Exception as e:
+        print(f"Discord error: {e}")
+
 
 @app.route("/session", methods=["POST"])
 def session():
 
     data = request.json
+
+    print(json.dumps(data, indent=4))
 
     with open(DATA_FILE, "r", encoding="utf-8") as f:
         sessions = json.load(f)
@@ -35,3 +54,6 @@ def home():
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
+
+
+
